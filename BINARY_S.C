55446703f837789ba54f27bfb8ba2763987be6fa
Devanarayanan.S.B <devan20034014@gmail.com>
2025-10-03 11:46:06 +0530

    Update BINARY_S.C

--- BINARY_S.C
+++ BINARY_S.C
@@ -1,82 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-struct node {
-    int key;
-    struct node *left, *right;
+struct node
+{
+int key;
+struct node*left,*right;
 };
-
-
-struct node* newnode(int item) {
-    struct node* temp = (struct node*)malloc(sizeof(struct node));
-    temp->key = item;
-    temp->left = temp->right = NULL;
-    return temp;
+struct node*newnode(int item)
+{
+struct node*temp=(struct node*)malloc(sizeof(struct node));
+temp->key=item;
+temp->left=temp->right=NULL;
+return temp;
 }
-
-
-struct node* insert(struct node* node, int key) {
-    if (node == NULL)
-        return newnode(key);
-
-    if (key < node->key)
-        node->left = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
-
-    return node;
+struct node*insert(struct node*node,int key)
+{
+if(node==NULL)
+return newnode(key);
+if(key<node->key)
+node->left=insert(node->left,key);
+else if(key>node->key)
+node->right=insert(node->right,key);
+return node;
 }
-
-
-void preorder(struct node* root) {
-    if (root != NULL) {
-        printf("%d ", root->key);
-        preorder(root->left);
-        preorder(root->right);
-    }
+void preorder(struct node*root)
+{
+if(root!=NULL)
+{
+printf("%d\n",root->key);
+preorder(root->left);
+preorder(root->right);
 }
-
-
-void inorder(struct node* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->key);
-        inorder(root->right);
-    }
 }
-
-
-void postorder(struct node* root) {
-    if (root != NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->key);
-    }
+void inorder(struct node*root)
+{
+if(root!=NULL)
+{
+inorder(root->left);
+printf("%d\n",root->key);
+inorder(root->right);
 }
-
-
-int main() {
-    struct node *root = NULL;
-
-   
-    root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
-
-   
-    printf("Preorder traversal of Binary Search Tree:\n");
-    preorder(root);
-
-    printf("\nInorder traversal of Binary Search Tree:\n");
-    inorder(root);
-
-    printf("\nPostorder traversal of Binary Search Tree:\n");
-    postorder(root);
-
-    return 0;
+}
+void postorder(struct node*root)
+{
+if(root!=NULL){
+postorder(root->left);
+postorder(root->right);
+printf("%d\n",root->key);
+}
+}
+void main()
+{
+struct node*root=NULL;
+clrscr();
+root=insert(root,50);
+insert(root,30);
+insert(root,20);
+insert(root,40);
+insert(root,70);
+insert(root,60);
+insert(root,80);
+printf("preorder traversal of binary search tree\n");
+preorder(root);
+printf("inorder traversal of binary search tree\n");
+inorder(root);
+printf("postorder traversal of binary search tree\n");
+postorder(root);
+getch();
 }
